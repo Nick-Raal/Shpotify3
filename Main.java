acdaf9ece451178d1ab6b5f20d5226b612e6e622
Nick-Raal <nicholasraal@gmail.com>
2022-05-11 23:45:24 -0700

    i think there might be some issues with javafx but i'm working on it

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
-public class Main {
-    public static void main(String args[]){
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import javafx.application.Application;
 
+
+public class Main extends Application{
+    public static void main(String args[]){
+        launch(args);
+    }
+
+    public void start(Stage primaryStage){
+        primaryStage.setTitle("test");
+
+        WebView webView = new WebView();
+
+        webView.getEngine().load("https://www.youtube.com/embed/1cGmWPI5TaI");
+        VBox vbox = new VBox(webView);
+        Scene scene = new Scene(vbox, 960, 600);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
