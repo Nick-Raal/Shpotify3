f75025b90dd41ad5f1006960f758ed7867abd19e
Nick-Raal <nicholasraal@gmail.com>
2022-05-15 23:26:42 -0700

    added some cool layout stuff + 3d saul goodman

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
@@ -13,19 +14,23 @@ import java.net.URL;
 
 
 public class Main extends Application{
-    public static void main(String args[]){
+    public static void main(String[] args){
         launch(args);
     }
 
     public void start(Stage primaryStage){
         primaryStage.setTitle("Shpotify 3");
 
-        //WebView webView = new WebView();
-        //webView.getEngine().load("https://www.youtube.com/embed/P6bVl47kdNk");
-        Text t = new Text(480, 300, "Shpotify 3");
-        t.setFill(Color.GREEN);
-        Group root = new Group(t);
+        WebView webView = new WebView();
+        webView.getEngine().load("https://www.youtube.com/embed/4kAevo9J_jo");
+        Text t = new Text(8, 48, "Shpotify 3 - Bigger, Better, Stronger, Faster");
+        t.setStyle("-fx-font: 48 arial;");
+        t.setFill(Color.grayRgb(135));
+        BorderPane root = new BorderPane();
+        root.setTop(t);
+        root.setCenter(webView);
         Scene scene = new Scene(root, 960, 600);
+        scene.setFill(Color.BLACK);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
